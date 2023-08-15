6014974a5b46cc8b16c7429be1bcef53bfab4e80
Caetano <Caetanojob05@gmail.com>
2023-08-15 19:40:49 -0300

    BASIC-ATIVIDADE-15/08/23

---
+++ POO-BASIC-ADS2/src/Atividade1.java
@@ -0,0 +1,22 @@
+public class Atividade1 {
+    public static void main(String[] args) {
+
+
+        //EXERCICIOS
+
+        byte per = 77;
+
+        double pre = 8.9876543;
+
+        int cart = 1234567;
+
+        char nota = 'O';
+
+        boolean habili = true;
+
+        float zona = 94.73239473240349234723847283374f;
+
+        //FIMEXERCICIOS
+
+    }
+}

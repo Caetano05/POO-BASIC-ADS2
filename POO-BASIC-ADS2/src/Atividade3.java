502e2f07b12c15f12d6062935488a587231f33e0
Caetano <Caetanojob05@gmail.com>
2023-08-15 21:57:37 -0300

    Calculadora-BASIC

---
+++ POO-BASIC-ADS2/src/Atividade3.java
@@ -0,0 +1,16 @@
+public class Atividade3 {
+    public static void main(String[] args) {
+
+
+        estring texto = "ola mundo";
+        int tamanho = texto.length();
+        if (tamanho <50 ){
+            System.out.println("Frase muito longa");
+        }if (tamanho =>50){
+            System.out.println("Frase muito curta");
+        }
+
+        string frase = ("Desenvolvimento é muito legal");
+
+    }
+}

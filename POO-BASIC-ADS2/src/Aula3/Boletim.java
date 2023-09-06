bb627423012139fd908aae953f4ca0787b909feb
Caetano <Caetanojob05@gmail.com>
2023-09-05 21:23:58 -0300

    AulaLogica

---
+++ POO-BASIC-ADS2/src/Aula3/Boletim.java
@@ -0,0 +1,17 @@
+public class Boletim {
+
+   String aluno;
+
+    public String verificaAlunoAprovado(String aluno,Integer mediaGeral, Boolean frequancia) {
+        if (frequancia <70) {
+            return ("reprovado");
+        }
+        if (mediaGeral >= 7) {
+            return ("O aluno " +aluno+ " foi APROVADO");
+        } else if (mediaGeral >= 5) {
+           return ("O aluno " +aluno+ " foi para RECUPERACAO");
+        } else {
+            return ("O aluno " +aluno+ " foi REPROVADO");
+        }
+    }
+}
---
+++ POO-BASIC-ADS2/src/Aula3/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno();
+        aluno.nome = "Bruno Kurzawe";
+
+        Disciplina matematica = new Disciplina();
+        matematica.adicionaNota(9);
+        matematica.adicionaNota(7);
+
+        Disciplina portugues = new Disciplina();
+        portugues.adicionaNota(8);
+        portugues.adicionaNota(8);
+
+
+
+        aluno.disciplinas.add(matematica);
+        aluno.disciplinas.add(portugues);
+
+        Integer notaGeral = aluno.calcularMediaGeral();
+        System.out.println("O aluno "+aluno.nome+ " tem média geral "+ notaGeral);
+
+
+        Boletim boletim = new Boletim();
+        String mensagemAprovado(aluno01.nomme, mediaGeralAluno);
+
+        System.out.println(mensagemAprovacao);
+
+
+
+    }
+    public Integer calcularfrequancia(){
+        Integer totalpresencas =0
+                for(Boolean presenca :frequancia){
+                    if (presenca.equals(true)){
+                        totalpresencas++
+                    }
+                }
+                Integer totalAulas = frequencia.size();
+                IntegerfrequnciaFinal = (totalpresencas * 100)/totalAulas;
+
+                return frequnciaFinal;
+    }
+
+}
